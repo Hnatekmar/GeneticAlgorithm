eec2e4ae2e419dcbab04bff0b94606c2e22ebfb4
Martin Hnátek <m.hnatma@gmail.com>
2017-05-07 15:10:24 +0200

    Added basic drawing code for GA visualization. This includes new travis although I am not sure if it will work.

--- source/app.d
+++ source/app.d
@@ -1,5 +1,28 @@
 import std.stdio;
 import genetic;
+import dsfml.graphics;
+
+void draw()
+{
+    auto window = new RenderWindow(VideoMode(800,600),"Genetický algoritmus");
+    auto circle = new CircleShape(100);
+    circle.fillColor = Color.Green;
+    while (window.isOpen())
+    {
+        Event event;
+
+        while(window.pollEvent(event))
+        {
+            if(event.type == event.EventType.Closed)
+            {
+                window.close();
+            }
+        }
+        window.clear();
+        window.draw(circle);
+        window.display();
+    }
+}
 
 int main(string[] argv)
 {
