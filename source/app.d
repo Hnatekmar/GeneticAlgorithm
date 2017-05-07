785c5221a5843ca946569ba7680094453ec80494
Ales Lerch <ales.lerch@gmail.com>
2017-05-08 01:06:01 +0200

    working on cli

--- source/app.d
+++ source/app.d
@@ -1,5 +1,6 @@
-import std.stdio;
 import genetic;
+import std.stdio;
+import std.getopt;
 import dsfml.graphics;
 
 void draw()
@@ -24,7 +25,31 @@ void draw()
     }
 }
 
+void getoptions(string[] args)
+{
+  string data = "";
+	int count_epoch = 0;
+	int mutation_number = 0;
+	float probability = 0.0;
+  auto helpInformation = getopt(
+    args,
+    "epoch|e","Counting epoch, number(int).", &count_epoch,
+    "mutation|m","Mutation, how large mutation should be(int).", &mutation_number,
+    "probability|p","Number(float) representing probability.", &probability,
+    "input|i","Input file with data.", &data);
+
+  if (helpInformation.helpWanted)
+  {
+    defaultGetoptPrinter("Generic algorithm for coloring pictures with geometric objects.\n",
+      helpInformation.options);
+  }
+}
+
 int main(string[] argv)
 {
-    return 0;
+	if (argv.length > 1)
+	{
+		getoptions(argv);
+	}
+	return 0;
 }
