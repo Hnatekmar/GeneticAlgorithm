4886cf84b28124abf85e361660c2637055452965
Martin Hnátek <m.hnatma@gmail.com>
2017-06-05 21:29:46 +0200

    Minor rewrite of GA. Now it caches fitness instead of recalculating it every time and fitness calculation is performed only when necessary.

--- source/Genetic.d
+++ source/Genetic.d
@@ -10,46 +10,63 @@ Individual!fitness getFittest(alias fitness)(Individual!(fitness)[] population)
 
 Individual!fitness tournamentSelection(alias fitness)(Individual!fitness[] population, size_t tournamentSize)
 {
-	Individual!fitness[] newPopulation;
-	foreach(i; 0 .. tournamentSize)
-	{
-		newPopulation ~= population[uniform(0, population.length)];
-	}
-	return newPopulation.getFittest();
+    import std.range;
+	randomShuffle(population);
+	return population.drop(tournamentSize).getFittest();
+}
+
+Individual!fitness rouletteSelection(alias fitness)(Individual!fitness[] population)
+{
+    import std.algorithm.iteration;
+    import std.algorithm.sorting;
+    population.sort!"a.fitness < b.fitness";
+    double sum = 0.0;
+    foreach(ref individual; population)
+    {
+        sum += individual.fitness;
+    }
+    auto gate = uniform!"[]"(0.0, sum);
+    import std.stdio;
+    foreach(ref individual; population)
+    {
+        gate -= individual.fitness;
+        if(gate <= 0.0) return individual;
+    }
+    return population[$ - 1];
 }
 
 Individual!fitness[] evolvePopulation(alias fitness)(Individual!fitness[] population,float mutation)
 {
 	Individual!fitness[] newPopulation;
-	newPopulation ~= population.getFittest!fitness();
+	newPopulation ~= population.getFittest;
 	foreach(i; 1..population.length)
 	{
-		auto a = population.tournamentSelection!fitness(12);
-		auto b = population.tournamentSelection!fitness(12);
+		auto a = population.tournamentSelection!fitness(5);
+		auto b = population.tournamentSelection!fitness(5);
 		newPopulation ~= a.crossover(b).mutate(mutation);
 	}
 	return newPopulation;
 }
-
 
 Individual!fitness geneticAlgorithm(alias fitness, alias print)(size_t genomSize, double requiredFitness,
                                                                 size_t populationSize, float mutationRate)
 {
 	import std.stdio: writeln;
 	Individual!fitness[] current;
-	foreach(i; 0..populationSize)
+	foreach(i; 1..populationSize)
 	{
 		current ~= new Individual!fitness(genomSize);
 	}
 	size_t generationNumber = 0;
+	double bestFitness = double.max;
 	while(current.getFittest.fitness > requiredFitness)
 	{
 		current = current.evolvePopulation!fitness(mutationRate);
-		if(generationNumber % 10 == 0)
-		{
-            auto fittest = current.getFittest!fitness();
-            writeln("===== Generation: ", generationNumber, " =====");
-            writeln("Fitness: ", fittest.fitness);
+        auto fittest = current.getFittest!fitness();
+        if(fittest.fitness < bestFitness)
+        {
+            writeln(generationNumber, " => ", fittest.fitness);
+            bestFitness = fittest.fitness;
         }
 		generationNumber += 1;
 	}
--- source/Individual.d
+++ source/Individual.d
@@ -11,54 +11,63 @@ class Individual(alias fitnessFn)
 	{
 		Mt19937 gen;
 		BitArray representation;
+		double calculatedFitness;
 	}
+
+    this(BitArray array, bool recalculateFitness = true)
+    {
+        representation = array;
+        if(recalculateFitness)
+            calculatedFitness = fitnessFn(array);
+		auto ct = Clock.currTime();
+		gen.seed(cast(uint)(ct.toUnixTime()));
+    }
 
 	this(size_t size)
 	{
-		auto ct = Clock.currTime();
-		gen.seed(cast(uint)(ct.toUnixTime()));
 		foreach(i; 0..size)
 		{
-			representation ~= uniform(0, 2, gen) == 1;
+			representation ~= uniform(0, 2) == 0;
 		}
+		calculatedFitness = fitnessFn(representation);
 	}
 
 	Individual mutate(float mutationRate)
 	{
-		Individual!fitnessFn newIndividual = new Individual!fitnessFn(1);
-		newIndividual.representation = representation.dup();
+		auto newRepresentation = representation.dup();
 		foreach(i; 0..representation.length)
 		{
 			if(mutationRate < uniform01(gen))
 			{
-				newIndividual.representation[i] = uniform(0, 2, gen) == 0;
+				newRepresentation[i] = uniform(0, 2, gen) == 0;
 			}
 		}
+		assert(newRepresentation.length() == representation.length());
+		Individual!fitnessFn newIndividual = new Individual!fitnessFn(newRepresentation);
 		return newIndividual;
 	}
 
 	Individual crossover(Individual individual)
 	{
-		Individual!fitnessFn newIndividual = new Individual!fitnessFn(1);
-		//size_t gate = uniform(1, individual.representation.length - 1);
 		size_t gate = individual.representation.length / 2;
 		bool[] bitArr;
 		foreach(i; 0 .. representation.length)
 		{
 			bitArr ~= i <= gate ? representation[i] : individual.representation[i];
 		}
-		newIndividual.representation = BitArray(bitArr);
+		assert(bitArr.length == representation.length());
+		Individual!fitnessFn newIndividual = new Individual!fitnessFn(BitArray(bitArr), false);
 		return newIndividual;
 	}
 
 	@property double fitness()
 	{
-		return fitnessFn(representation);
+	    return calculatedFitness;
 	}
 
-	@property BitArray genome()
+	@property ref BitArray genome()
 	{
-		return this.representation;
+		return representation;
 	}
 }
 
