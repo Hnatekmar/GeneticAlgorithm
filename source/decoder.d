17a033483375381d51a0feca3404a9881c814101
Zdeněk Biberle <z.biberle@gmail.com>
2017-06-11 00:03:21 +0200

    Gray code decoder

--- source/decoder.d
+++ source/decoder.d
@@ -5,7 +5,94 @@ import std.range;
 import std.meta;
 import std.conv;
 
-T decodeBits(T)(ref BitArray arr, size_t offset, size_t length)
+// Manipulating bits
+ubyte getBit(ubyte x, int n) {
+    return (x & (1 << n)) >> n;
+}
+
+ubyte withBit(ubyte x, int n, ubyte val) {
+    if (val) x |= (1 << n);
+    else x &= ~(1 << n);
+    return x;
+}
+
+ubyte setBit(ref ubyte x, int n, ubyte val) {
+    x = x.withBit(n, val);
+    return x;
+}
+
+unittest {
+    assert(0.getBit(0) == 0);
+    assert(1.getBit(0) == 1);
+    assert(2.getBit(0) == 0);
+    assert(3.getBit(0) == 1);
+    assert(0.getBit(1) == 0);
+    assert(1.getBit(1) == 0);
+    assert(2.getBit(1) == 1);
+    assert(3.getBit(1) == 1);
+    assert(0.getBit(2) == 0);
+    assert(1.getBit(2) == 0);
+    assert(2.getBit(2) == 0);
+    assert(3.getBit(2) == 0);
+    assert(4.getBit(2) == 1);
+    assert(5.getBit(2) == 1);
+    assert(6.getBit(2) == 1);
+    assert(7.getBit(2) == 1);
+
+    assert(0.withBit(0, 1) == 1);
+    assert(1.withBit(0, 0) == 0);
+    assert(2.withBit(0, 1) == 3);
+    assert(3.withBit(0, 0) == 2);
+
+    foreach (ubyte i; 0..64) {
+        ubyte x = i;
+        x.setBit(0, 1);
+        assert(x == i.withBit(0, 1));
+    }
+}
+
+/**
+ * This decodes a single gray code number
+ */
+ubyte decodeGray(ubyte bits)(ubyte x) {
+    ubyte decoded = 0.withBit(bits - 1, x.getBit(bits - 1));
+
+    foreach_reverse(bit; 0..(bits - 1)) {
+        decoded.setBit(bit, decoded.getBit(bit + 1) ^ x.getBit(bit));
+    }
+
+    return decoded;
+}
+
+unittest {
+    assert(decodeGray!4(0) == 0);
+    assert(decodeGray!4(1) == 1);
+    assert(decodeGray!4(2) == 3);
+    assert(decodeGray!4(3) == 2);
+    assert(decodeGray!4(4) == 7);
+    assert(decodeGray!4(5) == 6);
+    assert(decodeGray!4(6) == 4);
+    assert(decodeGray!4(7) == 5);
+}
+
+/**
+ * This is a statically built table that converts
+ * bytes in gray code to binary
+ */
+template grayCodeTable(ubyte bits) {
+    static ubyte[1 << bits] grayCodeTable;
+    static this() {
+        foreach(ubyte i; 0..1 << bits) {
+            grayCodeTable[i] = decodeGray!bits(i);
+        }
+    }
+}
+
+/**
+ * This decodes a single value from a BitArray.
+ * Right now it only does ubytes, because I'm lazy.
+ */
+T decodeBits(T = ubyte, size_t length)(ref BitArray arr, size_t offset)
 in {
     assert(length <= T.sizeof * 8, "Result of decodeBits must fit in the result type");
 }
@@ -28,7 +115,7 @@ body {
         result |= (data[1] & secondMask) << firstAvailable;
     }
 
-    return cast(T) result;
+    return grayCodeTable!length[result];
 }
 
 unittest {
@@ -40,19 +127,21 @@ unittest {
             false, true,  true,  false, false, false, false, false, false, false, false, false, false, false, false, false,
     ]);
 
-    assert(arr.decodeBits!ubyte(0, 1) == 1);
-    assert(arr.decodeBits!ubyte(0, 2) == 3);
-    assert(arr.decodeBits!ubyte(0, 3) == 7);
-    assert(arr.decodeBits!ubyte(0, 4) == 7);
-    assert(arr.decodeBits!ubyte(0, 5) == 23);
-    assert(arr.decodeBits!ubyte(3, 8) == 2);
-    assert(arr.decodeBits!ubyte(4, 8) == 1);
-    assert(arr.decodeBits!ubyte(61, 8) == 48);
-    assert(arr.decodeBits!ubyte(62, 8) == 24);
-    assert(arr.decodeBits!ubyte(63, 8) == 12);
-    assert(arr.decodeBits!ubyte(64, 8) == 6);
-    assert(arr.decodeBits!ubyte(65, 8) == 3);
+    assert(arr.decodeBits!(ubyte, 1)(0) == 1);
+    assert(arr.decodeBits!(ubyte, 2)(0) == 2);
+    assert(arr.decodeBits!(ubyte, 3)(0) == 5);
+    assert(arr.decodeBits!(ubyte, 4)(0) == 5);
+    assert(arr.decodeBits!(ubyte, 5)(0) == 26);
+    assert(arr.decodeBits!(ubyte, 8)(3) == 3);
+    assert(arr.decodeBits!(ubyte, 8)(4) == 1);
+    assert(arr.decodeBits!(ubyte, 8)(61) == 32);
+    assert(arr.decodeBits!(ubyte, 8)(62) == 16);
+    assert(arr.decodeBits!(ubyte, 8)(63) == 8);
+    assert(arr.decodeBits!(ubyte, 8)(64) == 4);
+    assert(arr.decodeBits!(ubyte, 8)(65) == 2);
 }
+
+
 
 /**
  * Generic decoder for extracting data
@@ -63,7 +152,7 @@ mixin template decoder(string name, string startName, size_t fieldOffset, T...)
     {
         import std.format : format;
 
-        mixin(q{auto %s = decodeBits!%s(%s, %s + %s, %s);}.format(T[2], T[0], name, startName, fieldOffset, T[1]));
+        mixin(q{auto %s = decodeBits!(%s, %s)(%s, %s + %s);}.format(T[2], T[0], T[1], name, startName, fieldOffset));
         mixin decoder!(name, startName, fieldOffset + T[1], T[3..$]);
     }
 }
@@ -78,7 +167,7 @@ unittest {
             "ubyte", 4, "d");
 
     assert(a == 1);
-    assert(b == 2);
-    assert(c == 5);
-    assert(d == 13);
+    assert(b == 3);
+    assert(c == 6);
+    assert(d == 9);
 }
